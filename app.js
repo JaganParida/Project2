9bf9f3a925f8330f3973284b65725acb0ce5e123
JaganParida <jaganparida39064@gmail.com>
2024-10-18 23:47:45 +0530

    add new button

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ h2.addEventListener("click", () => {
   h2.style.color = "yellow";
   h2.style.backgroundColor = "blueviolet";
 });
+
+let btn = document.createElement("button");
+btn.innerText = "click me";
+let body = document.querySelector("body");
+body.append(btn);
